7e9706a8aab469950208b27eb0d0b806fa8b946d
Jeffrey Evans <jeffreyevans.qcec@aol.com>
2022-02-11 14:01:03 -0500

    Migration came back empty

---
+++ EntityFrameworkCore-Tutorial/Migrations/20220211190000_Added order.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+// if you dont have anything in the up or the down methods then something went wrong with the migration process
+namespace EntityFrameworkCore_Tutorial.Migrations
+{
+    public partial class Addedorder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ EntityFrameworkCore-Tutorial/Models/Order.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// when adding a new migration put something descriptive about the new migration in the ""
+/// </summary>
+namespace EntityFrameworkCore_Tutorial.Models {
+    class Order {
+
+        public int Id { get; set; } // primary key
+        [Required]
+        [StringLength(80)]
+        public string Description { get; set; }
+        [Column(TypeName = "decimal(11,2)")]
+        public decimal Total { get; set; }
+
+        public int CustomerId { get; set; } // foreign key : if it has another name as a customer in another table (class) and Id at the end of it then the entity framework assigns this property as a foregin key
+        public virtual Customer Customer { get; set; } // marking a property as virtual it marks it in the class but not in the table
+
+    }
+}
